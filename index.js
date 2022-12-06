4025f424f35f1c40ef7df6d799624f0d66fff160
Hakeem <baighakeemyar@gmail.com>
2022-12-06 20:57:26 +0500

    edited index.js file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
+//var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently : " + dt.myDateTime());
+ res.end('hello world'); 
+//res.write("The date and time are currently : " + dt.myDateTime());
 }).listen(8081); 
