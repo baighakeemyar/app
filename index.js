cdfcc33cd753ec8461bc36b74dc9f18dc249dc0a
Hakeem <baighakeemyar@gmail.com>
2022-12-06 21:31:43 +0500

    added files

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var http = require('http');
-//var dt = require('./myfirstmodule');
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
- res.end('hello world'); 
-//res.write("The date and time are currently : " + dt.myDateTime());
-}).listen(8081); 
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 
