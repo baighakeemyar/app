cf3ef34ade81e21b71b3bea3c800cb274c169f3b
baighakeemyar <baighakeemyar@gmail.com>
2022-12-07 19:37:10 +0500

    deleted old files

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// import required modules
 var http = require('http');
 var fs = require('fs');
 
 // create server
 http.createServer(function (req, res) {
     // read file using fs build-in module
-        fs.readFile('index1.html', function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+    fs.readFile('index.html', function(err, data) {
+     if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
     });
-    
-}).listen(8084);
+  }).listen(8088); 
