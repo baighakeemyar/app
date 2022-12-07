0849344c039f44269429b63cde49dfb16daaf80a
baighakeemyar <baighakeemyar@gmail.com>
2022-12-07 18:59:35 +0500

    updated index files

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+// import required modules
 var http = require('http');
-var url = require('url');
 var fs = require('fs');
 
+// create server
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(1234); 
+    // read file using fs build-in module
+        fs.readFile('index1.html', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+    
+}).listen(8084);
