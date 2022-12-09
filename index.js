dfa941de3eb2b7729d275fa9b129bd1a725b9305
baighakeemyar <baighakeemyar@gmail.com>
2022-12-09 21:13:32 +0500

    New branch first commit

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ var fs = require('fs');
 // create server
 http.createServer(function (req, res) {
     // read file using fs build-in module
-    fs.readFile('index.html', function(err, data) {
+    fs.appendFile('text.txt','This is asynch \n', function(ferr){
+      if(ferr) return console.error(ferr);
+      console.log("saved");
+  });
+    fs.readFile('index.html', function(err, dataaynch) {
      if (err) {
         res.writeHead(404, {'Content-Type': 'text/html'});
         return res.end("404 Not Found");
       }
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
+      res.write(dataaynch);
       return res.end();
+      
     });
+    console.log("running");
+    
   }).listen(8088); 
   console.log("server is running at localhost:8088");
+
+ 
